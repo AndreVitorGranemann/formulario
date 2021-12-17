2f553973283fa79b281f29347c93d6abe42b4125
andre <andregranemann@hotmail.com>
2021-12-16 23:38:10 -0300

    criando classe, conexão com banco de dados

--- formularioContratacao/models.py
+++ formularioContratacao/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Contratacao(models.Model):
+    id = models.AutoField(primary_key=True)
+    nomecompleto = models.CharField(max_length=200)
+    dataNascimento = models.IntegerField()
+    rg_cpf= models.TextField()
+    contaNubank = models.TextField()
+    estadoCivil = models.TextField()
+    numeroCarteiraTrabalho = models.TextField()
+    whatsappEmail =  models.TextField()
+    linkdlen = models.TextField()
+    load = models.DateTimeField(auto_now=True)
