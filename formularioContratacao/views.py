fd296c01dc54521992edce3b97ad25d23fba3e1e
andre <andregranemann@hotmail.com>
2021-12-18 01:28:41 -0300

    rotas front-end

--- formularioContratacao/views.py
+++ formularioContratacao/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Contratacao
 
-# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def sucesso(request):
+    return render(request,'sucesso.html')
+
