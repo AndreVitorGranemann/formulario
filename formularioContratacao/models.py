45147b8e066f0f855b1a7b43eb2fb9156290b86d
andre <andregranemann@hotmail.com>
2021-12-21 12:56:31 -0300

    commitando erros

--- formularioContratacao/models.py
+++ formularioContratacao/models.py
@@ -4,17 +4,8 @@ from datetime import datetime
 class Contratacao(models.Model):
     id = models.AutoField(primary_key=True)
     nomecompleto = models.CharField(max_length=200)
-<<<<<<< HEAD
-<<<<<<< HEAD
-    dataNascimento = models.IntegerField()
-=======
-    dataNascimento = models.DateField()
->>>>>>> 62b0197 (alteracao)
-    rg_cpf= models.TextField()
-=======
     dataNascimento = models.TextField()
     rg_cpf = models.TextField()
->>>>>>> 721b641 (texte)
     contaNubank = models.TextField()
     estadoCivil = models.TextField()
     numeroCarteiraTrabalho = models.TextField()
